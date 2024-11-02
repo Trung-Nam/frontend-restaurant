d849194e185c4108a3340c559351f285fa69e876
Trung Nam <trungnam.work@gmail.com>
2024-11-02 21:10:07 +0700

    Loading content animation

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -58,6 +58,7 @@ const AuthProvider = ({ children }) => {
 
     const authInfo = {
         user,
+        loading,
         createUser,
         signUpWithGmail,
         login,
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,14 +2,24 @@ import { Outlet } from "react-router-dom"
 import Navbar from "../components/Navbar"
 import "../App.css"
 import Footer from "../components/Footer"
+import { useContext } from "react"
+import { AuthContext } from "../contexts/AuthProvider"
+import Loading from "../components/Loading"
 const Main = () => {
+  const { loading } = useContext(AuthContext);
+
   return (
-    <div>
-      <Navbar />
-      <div className="min-h-screen">
-        <Outlet />
-      </div>
-      <Footer />
+    <div className="bg-primary-background">
+      {
+        loading ?
+          <Loading />
+          :
+          <>
+            <Navbar />
+            <Outlet />
+            <Footer />
+          </>
+      }
     </div>
   )
 }
