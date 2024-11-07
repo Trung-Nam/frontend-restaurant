fbd8fdde4c8111d4acaf4f2cc60b9e49dbb6a7be
Trung Nam <trungnam.work@gmail.com>
2024-11-07 16:20:05 +0700

    Order transactions history

--- src/pages/dashboard/Order.jsx
+++ src/pages/dashboard/Order.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import useAuth from '../../hooks/useAuth'
 import { useQuery } from '@tanstack/react-query';
+import { Link } from 'react-router-dom';
+
 const Order = () => {
     const { user } = useAuth();
     const token = localStorage.getItem('access-token');
-    
+
     const { refetch, data: orders = [] } = useQuery({
         queryKey: ['orders', user?.email],
         queryFn: async () => {
@@ -18,9 +20,10 @@ const Order = () => {
         },
         enabled: !!token, // Only run query if token is available
     });
-    
-console.log(orders);
 
+    const formatDate = (date) => {
+        return new Date(date).toLocaleDateString();
+    }
 
     return (
         <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
@@ -36,7 +39,68 @@ console.log(orders);
             </div>
 
 
+            {/* table */}
+            <div className="overflow-x-auto">
+                {
+                    orders.length > 0 ? (
+                        <table className="table">
+                            {/* head */}
+                            <thead className="bg-primary text-white rounded-sm">
+                                <tr>
+                                    <th>#</th>
+                                    <th>Order Date</th>
+                                    <th>Transaction ID</th>
+                                    <th>Quantity</th>
+                                    <th>Price</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
 
+                                {
+                                    orders?.map((item, index) => (
+                                        <tr key={item._id}>
+                                            <td>{index + 1}</td>
+                                            <td>
+                                                {formatDate(item?.createAt)}
+                                            </td>
+                                            <td>
+                                                {item?.transactionId}
+                                            </td>
+                                            <td>
+                                                {item?.quantity}
+                                            </td>
+                                            <td>
+                                                {item?.price}
+                                            </td>
+                                            <td>
+                                                {item?.status}
+                                            </td>
+
+                                            <th>
+                                                <Link className="btn btn-ghost btn-md text-soft-red">
+                                                    Contact
+                                                </Link>
+                                            </th>
+                                        </tr>
+                                    ))
+                                }
+
+
+                            </tbody>
+                            {/* foot */}
+                            <tfoot>
+                            </tfoot>
+                        </table>
+                    ) : (
+                        <div className="flex items-center justify-center">
+                            <h2 className="text-2xl">You have no transactions. <Link to="/menu" className="text-primary hover:underline">Order Now!!!</Link></h2>
+                        </div>
+                )
+                }
+
+            </div>
         </div>
     )
 }
