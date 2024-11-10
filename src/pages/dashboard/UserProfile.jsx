01e19731b557fcad293e8d4b1e1bb73919c3c9cf
Trung Nam <trungnam.work@gmail.com>
2024-11-10 18:56:46 +0700

    update user profile

--- src/pages/dashboard/UserProfile.jsx
+++ src/pages/dashboard/UserProfile.jsx
@@ -1,9 +1,11 @@
 import React, { useContext } from 'react'
 import { useForm } from "react-hook-form"
 import { AuthContext } from '../../contexts/AuthProvider'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify';
+import useAuth from '../../hooks/useAuth';
 const UserProfile = () => {
+    const { user } = useAuth();
     const { updateUserProfile } = useContext(AuthContext)
     const {
         register,
@@ -11,9 +13,7 @@ const UserProfile = () => {
         formState: { errors },
     } = useForm();
 
-    const location = useLocation();
     const navigate = useNavigate();
-    const from = location.state?.from?.pathname || "/";
 
     const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
     const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
@@ -36,7 +36,7 @@ const UserProfile = () => {
                 // Update user profile with the image URL
                 updateUserProfile(data.name, imageUrl)
                     .then(() => {
-                        navigate("/", { state: { message: '🦄 Profile updated successfully!' } });
+                        window.location.reload();
                     })
                     .catch((error) => {
                         toast.error("Error updating profile. Please try again.");
@@ -55,16 +55,22 @@ const UserProfile = () => {
         <div className='flex items-center justify-center h-screen'>
             <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                 <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
-                    <h3 className='font-bold'>Update Your Profile</h3>
+                    <h2 className='font-bold text-center text-3xl mb-4'>Your Profile</h2>
+                    <img
+                        src={user.photoURL ? user.photoURL : "https://i0.wp.com/digitalhealthskills.com/wp-content/uploads/2022/11/3da39-no-user-image-icon-27.png?fit=500%2C500&ssl=1"}
+                        alt="avatar"
+                        className='rounded-full w-40 h-40 mx-auto'
+                    />
+                    <h2 className='font-bold text-center text-3xl my-4'>{user?.name}</h2>
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Name</span>
+                            <span className="label-text">Update Your Name ?</span>
                         </label>
                         <input {...register("name")} type="text" placeholder="Your name" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Upload Photo</span>
+                            <span className="label-text">Upload Update Avatar ?</span>
                         </label>
 
                         {/* <input type="text" {...register("photoURL")} placeholder="Photo URL" className="input input-bordered" required /> */}
